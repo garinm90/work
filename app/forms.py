2ebbd30f45462a23af3b2af2baf2037902cc7a8c
Max Garin <maxgarin@gmail.com>
2018-12-11 11:30:38 -0500

    Set default value to 0 for simpler calculations

--- app/forms.py
+++ app/forms.py
@@ -73,10 +73,20 @@ class ImageUploadForm(FlaskForm):
 
 
 class QuoteForm(FlaskForm):
-    bubble_six = IntegerField('Bubble 6', widget=widgets.Input(input_type='number'))
-    bubble_nine = IntegerField('Bubble 9', widget=widgets.Input(input_type='number'))
-    bubble_fourteen = IntegerField('Bubble 14', widget=widgets.Input(input_type='number'))
-    puck_six = IntegerField('Puck 6', widget=widgets.Input(input_type='number'))
-    puck_molex_six = IntegerField('Puck 6 Molex', widget=widgets.Input(input_type='number'))
-    puck_nine = IntegerField('Puck 9', widget=widgets.Input(input_type='number'))
+    # Lights
+    bubble_six = IntegerField('Bubble 6', widget=widgets.Input(input_type='number'), default=0)
+    bubble_nine = IntegerField('Bubble 9', widget=widgets.Input(input_type='number'), default=0)
+    bubble_fourteen = IntegerField('Bubble 14', widget=widgets.Input(input_type='number'), default=0)
+    puck_six = IntegerField('Puck 6', widget=widgets.Input(input_type='number'), default=0)
+    puck_molex_six = IntegerField('Puck 6 Molex', widget=widgets.Input(input_type='number'), default=0)
+    puck_nine = IntegerField('Puck 9', widget=widgets.Input(input_type='number'), default=0)
+    long_nineteen = IntegerField('Long Wire 19', widget=widgets.Input(input_type='number'), default=0)
+    short_nineteen = IntegerField('Short Wire 19', widget=widgets.Input(input_type='number'), default=0)
+    green_nineteen = IntegerField('Green 19', widget=widgets.Input(input_type='number'), default=0)
+    ads_twentyfour = IntegerField('ADS 24', widget=widgets.Input(input_type='number'), default=0)
+    ads_thirtysix = IntegerField('ADS 36', widget=widgets.Input(input_type='number'), default=0)
     submit = SubmitField('Submit')
+
+    # Power Supplies
+    two_forty = IntegerField('240', widget=widgets.Input(input_type='number'), default=0)
+    three_twenty = IntegerField('320', widget=widgets.Input(input_type='number'), default=0)
