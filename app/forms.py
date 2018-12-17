a81848cd21703d6ab4a663ca2672cc0dbdacd944
Max Garin <maxgarin@gmail.com>
2018-12-17 08:18:43 -0500

    Add rest of the form

--- app/forms.py
+++ app/forms.py
@@ -112,7 +112,7 @@ class QuoteForm(FlaskForm):
 
 
 class CreateControllerForm(FlaskForm):
-    order = SelectField('Order')
+    order = SelectField('')
     customer = SelectField('Customer')
     t_one_thousand = StringField('T1000', default=0, validators=[DataRequired()])
     t_one_thousand_a = StringField('T1000A', default=0, validators=[DataRequired()])
@@ -127,5 +127,8 @@ class CreateControllerForm(FlaskForm):
     raspberry_pi = StringField('Pies', default=0, validators=[DataRequired()])
     tp_link = StringField('TP-Link', default=0, validators=[DataRequired()])
     spokes = StringField('Spokes', default=0, validators=[DataRequired()])
-    boxes = TextAreaField('Boxes', default='Enter boxes used...', validators=[DataRequired()])
+    boxes = TextAreaField('Boxes', render_kw={'placeholder': 'Enter boxes used...'}, validators=[DataRequired()])
+    phoenix_one_by_one = StringField('Phoenix 1x1', default=0, validators=[DataRequired()])
+    phoenix_one_by_two = StringField('Phoenix 1x2', default=0, validators=[DataRequired()])
+    phoenix_two_by_two = StringField('Phoenix 2x2', default=0, validators=[DataRequired()])
     submit = SubmitField('Submit')
