25fa53c1eb18bad4c7d8509eddb6910706fddd4b
Max Garin <maxgarin@gmail.com>
2018-12-18 12:11:37 -0500

    Add role and user roles table with M2M relationshp

--- app/models.py
+++ app/models.py
@@ -9,6 +9,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    roles = db.relationship('Role', secondary='user_roles')
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -18,6 +20,19 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+# Define the Role data-model
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+# Define the UserRoles association table
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
 
 
 @login.user_loader
