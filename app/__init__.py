73629480dd851aafa126acbefd4d3a80eb61763d
Max Garin <maxgarin@gmail.com>
2018-12-18 12:11:11 -0500

    Add flask-principal

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,9 @@ from config import Config
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
 from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_principal import Principal
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,5 +16,6 @@ login = LoginManager(app)
 login.login_view = 'login'
 images = UploadSet('images', IMAGES)
 configure_uploads(app, images)
+principals = Principal(app)
 
 from app import routes, models
