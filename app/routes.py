36e89e84ca55409a910a4407f1d6e5a338f6ebc6
Max Garin <maxgarin@gmail.com>
2018-12-19 15:58:58 -0500

    Movedload identity  from routes to init

--- app/routes.py
+++ app/routes.py
@@ -5,28 +5,13 @@ from app import app, db, images
 from app.forms import  ImageUploadForm, QuoteForm
 from app.models import User, Customer, Order, Image, Controller
 
-admin_permission = Permission(RoleNeed('admin'))
 
 @app.errorhandler(403)
 def page_not_found(e):
     flash(f'403 Forbidden {request.url}')
     session['redirected_from'] = request.url
-    return redirect(url_for('index'))
+    return redirect(url_for('main.index'))
 
-@identity_loaded.connect_via(app)
-def on_identity_loaded(sender, identity):
-    # Set the identity user object
-    identity.user = current_user
-
-    # Add the UserNeed to the identity
-    if hasattr(current_user, 'id'):
-        identity.provides.add(UserNeed(current_user.id))
-
-    # Assuming the User model has a list of roles, update the
-    # identity with the roles that the user provides
-    if hasattr(current_user, 'roles'):
-        for role in current_user.roles:
-            identity.provides.add(RoleNeed(role.name))
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
