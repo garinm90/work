3e9800d3d7623feeb2c3d150b7d01bf02839dbc6
Max Garin <maxgarin@gmail.com>
2018-12-20 11:52:30 -0500

    Additional table formating

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,7 +1,38 @@
-$(document).ready( function () {
+$(document).ready(function () {
     $('#order_table').DataTable({
-        "columnDefs": [
-            { "orderable": false, "targets": [5, 6], }
+
+        "columnDefs": [{
+                "orderable": false,
+                "targets": [5, 6],
+            },
+            {
+                type: "natural",
+                "targets": [0, 3],
+            }
         ]
     });
-} );
+    $('#customer_jobs').DataTable({
+        "columnDefs": [{
+                "orderable": false,
+                "targets": [ 3, 4, 6, 7, 8, 9],
+            },
+            {
+                type: "natural",
+                "targets": [0, 4]
+            }
+        ]
+    });
+    $('#controller_list').DataTable({
+        'columnDefs': [{
+            "orderable": false,
+            "targets": [5]
+        }
+        ]
+    });
+    $('#customer_list').DataTable({
+        'columnDefs': [{
+            "orderable": false,
+            "targets": [1, 2, 3, 4]
+        }]
+    })
+});
